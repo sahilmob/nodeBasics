9408e87ce846449ee6c68945da339216a3c85f22
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-01 18:34:55 +0200

    add logic to create text file and redirect user on form submit

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const http = require('http');
-
+const fs = require('fs')
 
 const server = http.createServer((req, res) => {
   const url = req.url;
-
+  const method = req.method;
   if (url === '/') {
     res.write('<html>');
     res.write('<head><title>Enter Message</title></head>');
     res.write('</head>');
     res.write('<body><form action="/message" method="POST"><input name="message" type="text"><button type="submit">Submit</button></form></body>');
     res.write('</html>');
+    return res.end();
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync('message.txt', 'Dummy');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
     return res.end();
   }
   res.setHeader('Content-Type', 'text/html');
