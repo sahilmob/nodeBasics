98365f341c1baa3e6a7f3f741aa164ca257c1e7c
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-02 13:49:51 +0200

    add 404 page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const path = require('path');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -11,7 +12,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 });
 
 app.listen(3000)
